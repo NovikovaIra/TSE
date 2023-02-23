ab5f4c65a0e752b6599786c4e4d5c190bea71662
Irina Novikova <novikova.mrr@gmail.com>
2023-02-23 20:26:58 +0300

    Added tabulation

---
+++ TSE.js
@@ -0,0 +1,20 @@
+function openCity(evt, cityName) {
+    // Объявить все переменные
+    var i, tabcontent, tablinks;
+  
+    // Получить все элементы с помощью class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Получить все элементы с помощью class="tablinks" и удалить класс "active"
+   tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Показать текущую вкладку и добавить класс "active" по ссылке, открывшей вкладку
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
