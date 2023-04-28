01106eb682441d6b90d349325845bde6ec5267af
Irina Novikova <novikova.mrr@gmail.com>
2023-04-28 21:17:17 +0300

    Made a burger

--- TSE.js
+++ TSE.js
@@ -34,15 +34,10 @@ function off() {
   document.getElementById("overlay").style.display = "none";
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-//   console.log(scrollbar);
-//   document.querySelector('[href="#openModal"]').addEventListener('click', function () {
-//     document.body.style.overflow = 'hidden';
-//     document.querySelector('#openModal').style.marginLeft = scrollbar;
-//   });
-//   document.querySelector('[href="#close"]').addEventListener('click', function () {
-//     document.body.style.overflow = 'visible';
-//     document.querySelector('#openModal').style.marginLeft = '0px';
-//   });
-// });
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
